1ee89340cc4cbbff156941af2131338fc031440f
Boyang Yan <yanboyang713@gmail.com>
2021-10-21 14:47:14 +1100

    email client

--- +betterDefaults.el
+++ +betterDefaults.el
@@ -21,3 +21,38 @@
 (setq default-buffer-file-coding-system 'utf-8)
 ;; No additional confirmation required when closing emacs
 (setq confirm-kill-emacs nil)
+
+;;Projectile
+;;projectile-project-search-path '("~/Project/" "~/dotfiles/" "~/blog/content-org/")
+
+;;(setq vterm-module-cmake-args "-DUSE_SYSTEM_LIBVTERM=yes")
+;;(define-key vterm-mode-map [return] #'vterm-send-return)
+;;
+;;
+;;
+(after! (vterm evil-collection)
+  (add-hook!
+   'vterm-mode-hook
+   ;; evil-collection for vterm overrided some keymaps defined by tmux-pane
+   (evil-collection-define-key 'insert 'vterm-mode-map
+     (kbd "C-h") (lambda () (interactive) (tmux-pane--windmove
+                                      "left"
+                                      "tmux select-pane -L"))
+     (kbd "C-j") (lambda () (interactive) (tmux-pane--windmove
+                                      "down"
+                                      "tmux select-pane -D"))
+     (kbd "C-k") (lambda () (interactive) (tmux-pane--windmove
+                                      "up"
+                                      "tmux select-pane -U"))
+     (kbd "C-l") (lambda () (interactive) (tmux-pane--windmove
+                                      "right"
+                                      "tmux select-pane -R")))
+   ;; change keymap to toggle sending escape to vterm
+   (evil-collection-define-key '(normal insert) 'vterm-mode-map
+     (kbd "C-c") 'vterm--self-insert
+     ;; for CLI emacs
+     (kbd "ESC <escape>") 'evil-collection-vterm-toggle-send-escape
+     ;; for GUI emacs
+     (kbd "M-<escape>") 'evil-collection-vterm-toggle-send-escape)
+   ;; send escape to vterm by default
+   (evil-collection-vterm-toggle-send-escape)))
--- +email.el
+++ +email.el
@@ -1,41 +1,45 @@
 ;;; ../../dotfiles/doom/+email.el -*- lexical-binding: t; -*-
 ;; mu4e setup
-;;(after! mu4e
-;;  (setq mu4e-root-maildir (expand-file-name "~/.local/share/mail/essb")
-;;        mu4e-get-mail-command "mbsync -a -c \"$XDG_CONFIG_HOME/isync/mbsyncrc\""
-;;        mu4e-index-update-in-background t
-;;        mu4e-use-fancy-chars t
-;;        mu4e-view-show-addresses t
-;;        mu4e-view-show-images t
-;;        mu4e-compose-format-flowed t
-;;        mu4e-compose-signature-auto-include nil
-;;        mu4e-view-use-gnus t
-;;        mu4e-change-filenames-when-moving t
-;;        message-send-mail-function 'smtpmail-send-it
-;;        message-citation-line-format "On %a %d %b %Y at %R, %f wrote:\n"
-;;        message-citation-line-function 'message-insert-formatted-citation-line
-;;        message-kill-buffer-on-exit t
-;;        org-mu4e-convert-to-html t)
-;;  (add-hook 'mu4e-compose-mode-hook 'turn-off-auto-fill)
-;;  (add-hook 'mu4e-compose-mode-hook (lambda() (use-hard-newlines -1))))
+(after! mu4e
+  (setq mu4e-root-maildir (expand-file-name "~/MailDir")
+        mu4e-get-mail-command "mbsync -a -c  ~/.config/mu4e/mbsyncr/mbsyncrc"
+        ;;mu4e-index-update-in-background t
+        mu4e-use-fancy-chars t
+        mu4e-view-show-addresses t
+        mu4e-view-show-images t
+        mu4e-compose-format-flowed t
+        mu4e-compose-signature-auto-include t
+        mu4e-change-filenames-when-moving t
+        mu4e-attachment-dir "~/Downloads"
+        sendmail-program (executable-find "msmtp")
+        send-mail-function #'smtpmail-send-it
+        ;;message-send-mail-function #'smtpmail-send-it
+        message-sendmail-f-is-evil t
+        message-sendmail-extra-arguments '("--read-envelope-from")
+        message-send-mail-function #'message-send-mail-with-sendmail
+
+        message-citation-line-format "On %a %d %b %Y at %R, %f wrote:\n"
+        message-citation-line-function 'message-insert-formatted-citation-line
+        message-kill-buffer-on-exit t
+        org-mu4e-convert-to-html t)
+  (add-hook 'mu4e-compose-mode-hook 'turn-off-auto-fill)
+  (add-hook 'mu4e-compose-mode-hook (lambda() (use-hard-newlines -1))))
 
 ;; Email alert
-;;(add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)
+(add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)
 
 ;; Setup email account
-;;(set-email-account! "essb"
-;;                    '((mu4e-sent-folder                 .       "/essb/Sent")
-;;                      (mu4e-drafts-folder               .       "/essb/Drafts")
-;;                      (mu4e-trash-folder                .       "/essb/Trash")
-;;                      (mu4e-refile-folder               .       "/essb/INBOX")
-;;                      (message-send-mail-function       .       smtpmail-send-it)
-;;                      (smtpmail-smtp-user               .       "45995wsp@eur.nl")
-;;                      (smtpmail-smtp-server             .       "localhost")
-;;                      (smtpmail-smtp-service            .       1025)
-;;                      (smtpmail-stream-type             .       nil)
-;;                      (user-mail-address                .       "spekkink@essb.eur.nl")
-;;                      (mu4e-update-interval             .       300))
-;;                    t)
+(set-email-account! "gmail"
+                    '((mu4e-sent-folder                 .       "/gmail/Sent")
+                      (mu4e-drafts-folder               .       "/gmail/Drafts")
+                      (mu4e-trash-folder                .       "/gmail/Trash")
+                      (mu4e-refile-folder               .       "/gmail/INBOX")
+                      ;;(message-send-mail-function       .       smtpmail-send-it)
+                      (smtpmail-smtp-user               .       "yanboyang713@gmail.com")
+                      (user-mail-address                .       "yanboyang713@gmail.com")
+                      (mu4e-compose-signature . "---\nBoyang Yan")
+                      (mu4e-update-interval             .       300))
+                    t)
 
 (after! org-msg
   (setq org-msg-default-alternatives nil))
